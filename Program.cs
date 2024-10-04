4d5a4dd02de01f110c0e0a09d391a9b428d4cac6
Naveen Manjappa <NManjappa@manh.com>
2024-10-04 08:17:14 +0100

    Adding swagger to the project

--- Program.cs
+++ Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options=>{});
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options=>{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API",Description="Keep track of your tasks",Version="v1"});    
+  });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("Middleware policy");
 
+
+
+
+app.MapGet("/todo", () => "Hello World from minimal api!");
+
+
+//Swagger
+if(app.Environment.IsDevelopment()){ 
+
+  app.UseSwagger();
+
+  app.UseSwaggerUI(c=>{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json","To DO API V1");
+  });
+}
 app.Run();
