6455c08e973bdead379809159b5a81c195efc916
Naveen Manjappa <NManjappa@manh.com>
2024-10-04 12:42:23 +0100

    Renaming the api title

--- Program.cs
+++ Program.cs
@@ -7,17 +7,12 @@ builder.Services.AddCors(options=>{});
 //Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options=>{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API",Description="Keep track of your tasks",Version="v1"});    
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API",Description="Making the pizzas you love!",Version="v1"});    
   });
 
 var app = builder.Build();
 
 app.UseCors("Middleware policy");
-
-
-
-
-app.MapGet("/todo", () => "Hello World from minimal api!");
 
 
 //Swagger
@@ -26,7 +21,10 @@ if(app.Environment.IsDevelopment()){
   app.UseSwagger();
 
   app.UseSwaggerUI(c=>{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json","To DO API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json","PizzaStore API V1");
   });
 }
+
+app.MapGet("/", () => "Hello World from minimal api!");
+
 app.Run();
