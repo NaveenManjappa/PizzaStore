d3d117183e46c4534f91851f3d7f30f94d96aad5
Naveen Manjappa <NManjappa@manh.com>
2024-10-04 13:31:06 +0100

    Adding the routes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,5 +27,16 @@ if(app.Environment.IsDevelopment()){
 }
 
 app.MapGet("/", () => "Hello World from minimal api!");
+
+//Pizza APIs
+app.MapGet("/pizzas/{id}",(int id)=> PizzaDB.GetPizza(id));
+
+app.MapGet("/pizzas",()=>PizzaDB.GetPizzas());
+
+app.MapPost("/pizzas",(Pizza pizza)=>PizzaDB.CreatePizza(pizza));
+
+app.MapPut("/pizzas",(Pizza pizza)=> PizzaDB.UpdatePizza(pizza));
+
+app.MapDelete("/pizzas/{id}",(int id)=> PizzaDB.RemovePizza(id));
 
 app.Run();
